08f7953e7c2f8f853805afb0dce5ff2f0777fc1c
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-01-20 15:48:22 +0300

    max-h-screen

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -7,7 +7,7 @@ import {
 } from "rakkasjs";
 import "./index.css";
 
-import React from "react";
+
 import { Sidebar } from "@/components/navigation/bars/sidebar";
 import Toaster from "@/components/wrappers/DefaltExportedToaster";
 import ErrorBoundaryComponent from "@/components/navigation/ErrorBoundaryComponent";
@@ -19,18 +19,18 @@ function Layout({ children }: LayoutProps) {
   // console.log(" page ctx ==== ",page_ctx.locals.pb)
   return (
     <ErrorBoundaryComponent>
-      <div className="w-full h-screen flex flex-col items-center ">
+      <div className="w-full h-screen overflow-y-hidden  flex flex-col items-center ">
         {/* <Head description={"Resume building assistant"} /> */}
         <ClientSuspense fallback={<div></div>}>
           <Nprogress
             isAnimating={location && location?.pending ? true : false}
           />
         </ClientSuspense>
-        <div className="w-full flex h-full gap-2">
+        <div className="w-full flex h-screen gap-2">
           <div className="min-w-[5%] w-fit flex h-full gap-2">
             <Sidebar />
           </div>
-          <div className="w-full flex flex-col gap-2 pt-2">
+          <div className="w-full max-h-[97vh] flex flex-col gap-2 pt-2">
 
             <div className="w-fit flex rounded-xl p-auto">
             <ClientSuspense fallback={<div className="h-5"></div>}>
